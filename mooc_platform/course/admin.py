4776edce3424ef72b7d7c3278acbd6d94a77cf22
Ashish Bhardwaj <erashish41@gmail.com>
2026-02-01 03:28:30 +0530

    custom admin

--- mooc_platform/course/admin.py
+++ mooc_platform/course/admin.py
@@ -7,18 +7,22 @@ from course.models import Course, Lesson, Enrollment, LessonProgress
 class CourseAdmin(admin.ModelAdmin):
     list_display = ["title", "created_at"]
     search_fields = ["title",]
+    list_filter = ["created_at"]
     
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
     list_display = ["title", "course", "created_at"]
     search_fields = ["title", "course__title"]
+    list_filter = ["course"]
     
-@admin.register
+@admin.register(Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
     list_display = ["user", "course", "enrolled_at"]
     search_fields = ["user__username", "course__title"]
+    list_filter = ["course"]
     
-@admin.register
+@admin.register(LessonProgress)
 class LessonProgressAdmin(admin.ModelAdmin):
     list_display = ["user", "lesson", "viewed", "viewed_at"]
     search_fields = ["user__username", "lesson__title"]
+    list_filter = ["lesson"]
--- mooc_platform/course/views.py
+++ mooc_platform/course/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from course.models import Course, Lesson, Enrollment, LessonProgress
 
 # Create your views here.
